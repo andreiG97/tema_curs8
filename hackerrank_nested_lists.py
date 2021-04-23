b013679e4a6fb122d07eec63d31cf62277ca02ce
Andrei G <andrei.grideanu@gmail.com>
2021-04-23 23:11:36 +0300

    Hacker_rank_exercise

---
+++ hackerrank_nested_lists.py
@@ -0,0 +1,29 @@
+n = int(input("Nr of students: "))
+list_nest = []
+list_grades = []
+result = []
+
+
+def creating_catalog():
+
+    for i in range(n):
+        name = input("Student name: ")
+        score = float(input("Grade: "))
+        list_nest2 = [name, score]
+        list_grades.append(score)
+        list_nest.append(list_nest2)
+    return list_nest
+
+
+def ordering_students(list_students):
+    list_grades.sort()
+    sorted_list = []
+    for i in list_students:
+        if i[1] == list_grades[1]:
+            result.append(i[0])
+        sorted_list = sorted(result, key=str.lower)
+    return sorted_list
+
+
+creating_catalog()
+print(ordering_students(list_nest))
